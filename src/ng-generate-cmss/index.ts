d8fd3d8ba48ea4553657f8840a3be9910b7b609e
jramirez <jramirez@rocksoftgt.dev>
2023-12-22 23:33:21 -0600

    Fix: Fixed move files

--- src/ng-generate-cmss/index.ts
+++ src/ng-generate-cmss/index.ts
@@ -1,5 +1,5 @@
-import {normalize, strings} from '@angular-devkit/core';
-import {apply, mergeWith, move, Rule, SchematicContext, template, Tree, url} from '@angular-devkit/schematics';
+import {strings} from '@angular-devkit/core';
+import {apply, mergeWith, Rule, SchematicContext, template, Tree, url} from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -8,7 +8,7 @@ export function ngGenerateCmss(_options: any): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         const sourceTemplates = url('./files');
         const sourceParametrizedTemplates = apply(sourceTemplates, [
-            move(normalize(_options.path)),
+            // move(normalize(_options.path)),
             template({
                 ..._options,
                 ...strings
